d68585bd3fdb317764b59abb23f1de50c4ced3eb
Murilo Júnior <silvamurilojr@gmail.com>
2025-02-01 23:23:39 -0300

    Animation Scroll

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function initTabNav() {
-
+function minhafuncao() {
   const jsfelino = document.querySelectorAll('.jsfelino li');
   const jsfelidescri = document.querySelectorAll('.jsfelidescri section');
 
@@ -20,4 +19,45 @@ function initTabNav() {
     });
   }
 }
-initTabNav();
+minhafuncao();
+
+function initscrollSoft() {
+  const linkInterno = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  linkInterno.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initscrollSoft();
+
+function initAnimationScroll() {
+  const sections = document.querySelectorAll('js-scroll');
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScrooll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if(isSectionVisible) {
+          section.classList.add('ativo');
+        }
+      })
+    }
+
+    animaScrooll();
+
+    window.addEventListener('scroll', animaScrooll);
+  }
+}
+initAnimationScroll();
